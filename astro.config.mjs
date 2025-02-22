3678c55bdf92cc85e5dc03294bfafd521afa2f3f
Hassan Adraou <adraou.hassan@outlook.com>
2025-02-22 21:06:50 +0100

    First_commit
    Spanish version

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+
+export default defineConfig({
+  i18n: {
+    defaultLocale: "es",
+    locales: ["es", "en", "fr", "ar"],
+    routing: {
+        prefixDefaultLocale: false
+    }
+  }
+})
